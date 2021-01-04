f53c88a38b433f191d8d80a364b6d11ef06b2527
shazia budhwani <mohtashim.siddiqui74@outlook.com>
2021-01-04 16:36:22 -0600

    theme

---
+++ MovengoCommon/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using MovengoCommon.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovengoCommon
+{
+    public class Customer
+    {
+        [Key]
+        public int UserId { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(100)]
+        public string LastName { get; set; }
+        [StringLength(100)]
+        public string Password { get; set; }
+        [StringLength(10)]
+        public string MobileCode { get; set; }
+        [StringLength(30)]
+        public string MobileNumber { get; set; }
+        [StringLength(30)]
+        public string EmailId { get; set; }
+        public virtual ICollection<Shipment> Shipments { get; set; }
+
+
+    }
+}
---
+++ MovengoCommon/Models/MovengoContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+
+namespace MovengoCommon.Models
+{
+    public partial class MovengoContext : DbContext
+    {
+        public MovengoContext()
+        {
+        }
+
+        public MovengoContext(DbContextOptions<MovengoContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Shipment> Shipments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                //entity.ToTable("Customer");
+
+                entity.HasIndex(e => e.UserId, "IX_Customer_UserId");
+
+                entity.HasIndex(e => e.EmailId, "IX_Customer_EmailId");
+
+            });
+            modelBuilder.Entity<Shipment>(entity =>
+            {
+                //entity.ToTable("Shipment");
+                entity.HasIndex(e => e.UserId, "IX_Shipment_UserId");
+                entity.HasIndex(e => e.ShipmentId, "IX_Shipment_ShipmentId");
+                entity.HasIndex(e => e.WayBillNumber, "IX_Shipment_WayBillNumber");
+
+                entity.HasOne(d => d.Customers)
+                    .WithMany(p => p.Shipments)
+                    .HasForeignKey(d => d.UserId);
+                //.HasConstraintName("FK_Shipment_UserId");
+
+            });
+        }
+    }
+}
